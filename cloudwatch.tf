e7ba25e0758e30e4cadcf457091a21e2455d62a1
Nick Doyle <nick.doyle@intelematics.com>
2019-12-30 16:55:20 +1100

    feat: Interpolation-only expressions are deprecated

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -13,14 +13,14 @@ PATTERN
 }
 
 resource "aws_cloudwatch_event_target" "main" {
-  rule = "${aws_cloudwatch_event_rule.main.name}"
-  arn  = "${module.ecr_scan_notify_lambda.function_arn}"
+  rule = aws_cloudwatch_event_rule.main.name
+  arn  = module.ecr_scan_notify_lambda.function_arn
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${module.ecr_scan_notify_lambda.function_name}"
+  function_name = module.ecr_scan_notify_lambda.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.main.arn}"
+  source_arn    = aws_cloudwatch_event_rule.main.arn
 }
